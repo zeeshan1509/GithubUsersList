4f39a6815c8a2a9b1346fb8d92344b3e464c2bc5
zeeshan1509 <zeerabbani1234@gmail.com>
2021-07-04 00:16:27 +0500

    first commit

---
+++ src/Appp.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import UserEffectApi from './component/useEffect'
+import './App.css'
+class Appp extends Component{
+
+
+  render(){
+    return(
+      <div>
+        <UserEffectApi/>
+      </div>
+      )
+  }
+}
+
+export default Appp;
